2a2c4cf0a28ad5af95f9e2b613c111e06734fcd7
Inweregbu Joseph Chukwuka <inwerejosic@protonmail.com>
2021-07-18 18:30:28 +0100

    After the final touch to task 1

--- Task_1.js
+++ Task_1.js
@@ -1,14 +1,14 @@
 //one
-function sumOfEvenAndOddInts(arr) {
-    let resArr = []; //array to store the values;
+function sum_of_EvenAndOdds(arr) {
+    let myArray = []; //array to store the values;
     arr.forEach(value => { //iterate over the values in the array;
         if (value % 2 === 0) { //if value is even
-            resArr[0] = resArr[0] ? resArr[0] + value : value; //if index 0 of resArr is empty, make it equal to the value, otherwise add current value to the value in index 0 of resArr;
+            myArray[0] = myArray[0] ? myArray[0] + value : value; //if index 0 of myArray is empty, make it equal to the value, otherwise add current value to the value in index 0 of resArr;
         } else { //if value is odd
-            resArr[1] = resArr[1] ? resArr[1] + value : value; //if index 1 of resArr is empty, make it equal to the value, otherwise add current value to the value in index 1 of resArr;
+            myArray[1] = myArray[1] ? myArray[1] + value : value; //if index 1 of myArray is empty, make it equal to the value, otherwise add current value to the value in index 1 of resArr;
         }
     });
-    return resArr; //return resArr;
+    return myArray; //return myArray;
 };
 
-document.getElementById("tasks").innerHTML = sumOfEvenAndOddInts([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+document.getElementById("tasks").innerHTML = sum_of_EvenAndOdds([2, 4, 10, 11, 7, 9, 5, 13, 23, 17, 27]);
